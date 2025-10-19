df09cb85cb7c9fb5c94a9ef7bb8847a8031b6093
Myat Thura Win <2019-miit-cse-036@miit.edu.mm>
2025-10-20 00:57:33 +0630

    firebase set up done

--- firebasetestapp/lib/main.dart
+++ firebasetestapp/lib/main.dart
@@ -1,12 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebasetestapp/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(const MyApp());
+
+Future <void> main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await testData();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+      testData();
 
       return MaterialApp(
           title: 'Events',
@@ -18,7 +32,7 @@ class MyApp extends StatelessWidget{
             body: Center(
               child: 
                 Text(
-                  "Hello World 🥰",
+                  "Retrieving data from Firestore 🔥",
                   style: TextStyle(fontSize: 32),
                 ),
             ),
@@ -30,3 +44,21 @@ class MyApp extends StatelessWidget{
    
 }
 
+
+Future<void> testData() async {
+
+  final db = FirebaseFirestore.instance;
+
+  final QuerySnapshot<Map<String,dynamic>> snapshot = 
+    await db.collection('event_details').get();
+
+  final List<QueryDocumentSnapshot<Map<String,dynamic>>> details = snapshot.docs;
+
+  for (final doc in details){
+    print('Document ID: ${doc.id}');
+    print('Document Data: ${doc.data()}');
+
+  }
+
+
+}
